ec704a4d9cf03d762fe5fcceb37e214314ed8b65
Harshitha2005415 <vaishyarajuharshitha@gmail.com>
2025-04-30 11:06:06 +0530

    Update Happy.java

--- Happy.java
+++ Happy.java
@@ -1,5 +1,20 @@
+import java.util.*;
 class Happy{
     public static void main(String[] args){
-        System.out.println("hi");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=happy(n);
+        if(a==1||a==7) System.out.print(true);
+        else System.out.print(false);
+    }
+    public static int happy(int n){
+        if(n<10) return n;
+        int k=0;
+        while(n>0){
+            int r=n%10;
+            k+=r*r;
+            n=n/10;
+        }
+    return happy(k);
     }
 }
